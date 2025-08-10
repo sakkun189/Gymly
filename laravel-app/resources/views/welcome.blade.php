d6e6356892708a003e96bd46ff5ec28cba65c535
sakkun189 <sakkun189@gmail.com>
2025-08-10 14:24:27 +0000

    仮ログイン機能の実装

--- laravel-app/resources/views/welcome.blade.php
+++ laravel-app/resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ja">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,15 +33,109 @@
                     @else
                         <a
                             href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
-                            Log in
-                        </a>
+                    <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+                        @if (Route::has('login'))
+                            <nav class="flex items-center justify-end gap-4">
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                                    >
+                                        ダッシュボード
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                                    >
+                                        ログイン
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                            新規登録
+                                        </a>
+                                    @endif
+                                @endauth
+                            </nav>
+                        @endif
+                    </header>
 
                         @if (Route::has('register'))
                             <a
-                                href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                            <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
+                                <h1 class="mb-1 font-medium">はじめましょう</h1>
+                                <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravelは非常に豊富なエコシステムを持っています。<br>まずは以下から始めることをおすすめします。</p>
+                                <ul class="flex flex-col mb-4 lg:mb-6">
+                                    <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:top-1/2 before:bottom-0 before:left-[0.4rem] before:absolute">
+                                        <span class="relative py-1 bg-white dark:bg-[#161615]">
+                                            <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border dark:border-[#3E3E3A] border-[#e3e3e0]">
+                                                <span class="rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A] w-1.5 h-1.5"></span>
+                                            </span>
+                                        </span>
+                                        <span>
+                                            <a href="https://laravel.com/docs" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
+                                                <span>ドキュメントを読む</span>
+                                                <svg
+                                                    width="10"
+                                                    height="11"
+                                                    viewBox="0 0 10 11"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    class="w-2.5 h-2.5"
+                                                >
+                                                    <path
+                                                        d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
+                                                        stroke="currentColor"
+                                                        stroke-linecap="square"
+                                                    />
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </li>
+                                    <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:bottom-1/2 before:top-0 before:left-[0.4rem] before:absolute">
+                                        <span class="relative py-1 bg-white dark:bg-[#161615]">
+                                            <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border dark:border-[#3E3E3A] border-[#e3e3e0]">
+                                                <span class="rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A] w-1.5 h-1.5"></span>
+                                            </span>
+                                        </span>
+                                        <span>
+                                            <a href="https://laracasts.com" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
+                                                <span>動画チュートリアルを見る（Laracasts）</span>
+                                                <svg
+                                                    width="10"
+                                                    height="11"
+                                                    viewBox="0 0 10 11"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    class="w-2.5 h-2.5"
+                                                >
+                                                    <path
+                                                        d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
+                                                        stroke="currentColor"
+                                                        stroke-linecap="square"
+                                                    />
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </li>
+                                </ul>
+                                <ul class="flex gap-3 text-sm leading-normal">
+                                    <li>
+                                        <a href="https://cloud.laravel.com" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                                            今すぐデプロイ
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
+                                {{-- Laravel Logo --}}
+                                // ...existing code...
+                            </div>
+                        </main>
+                    </div>
                                 Register
                             </a>
                         @endif
